6e974041aee8d697915e108cd0a3eec295e3dae6
bnb32 <brandon.benton@nrel.gov>
2025-09-04 07:17:50 -0600

    deprecating random forest code

--- phygnn/model_interfaces/__init__.py
+++ phygnn/model_interfaces/__init__.py
@@ -3,6 +3,6 @@
 operations, e.g. on-the-fly normalization during forward pass,
 one-hot-encoding, and saving/loading models.
 """
+
 from .phygnn_model import PhygnnModel
-from .random_forest_model import RandomForestModel
 from .tf_model import TfModel
