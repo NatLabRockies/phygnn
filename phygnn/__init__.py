cfc600af3fccbe92dec093381d22594b437d5e21
bnb32 <brandon.benton@nrel.gov>
2025-09-04 07:23:03 -0600

    remove RandomForestModel import from __init__.py

--- phygnn/__init__.py
+++ phygnn/__init__.py
@@ -6,11 +6,10 @@ import os
 from tensorflow.keras.utils import get_custom_objects
 
 from ._version import __version__
-
 from .base import CustomNetwork, GradientUtils
 from .layers import HiddenLayers, Layers
 from .layers.custom_layers import GaussianAveragePooling2D
-from .model_interfaces import PhygnnModel, RandomForestModel, TfModel
+from .model_interfaces import PhygnnModel, TfModel
 from .phygnn import PhysicsGuidedNeuralNetwork
 from .utilities import PreProcess, tf_isin, tf_log10
 
